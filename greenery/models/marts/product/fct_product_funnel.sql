17dc796f7cd68b4de9640a7c9d2ad071cc1a7fb5
Eike Rogall <eike.rogall@gmail.com>
2022-07-04 10:54:11 +0000

    updated funnel

--- greenery/models/marts/product/fct_product_funnel.sql
+++ greenery/models/marts/product/fct_product_funnel.sql
@@ -9,9 +9,9 @@ with session_events_agg as (
 )
 , agg_total as (
     select 
-        sum(page_view) as total_page_views
-        , sum(add_to_cart) as total_add_to_carts
-        , sum(checkout) as total_checkouts
+        sum(case when page_view >= 1 then 1 else 0 end) as total_page_views
+        , sum(case when add_to_cart >= 1 then 1 else 0 end) as total_add_to_carts
+        , sum(case when checkout >= 1 then 1 else 0 end) as total_checkouts
     from session_events_agg
 )
 
@@ -22,10 +22,10 @@ select
     , agg_total.total_page_views
     , agg_total.total_add_to_carts
     , agg_total.total_page_views - agg_total.total_add_to_carts AS diff_view_to_cart
-    , ROUND((agg_total.total_add_to_carts / agg_total.total_page_views) * 100,2) AS pct_view_to_cart
+    , ROUND((agg_total.total_add_to_carts::numeric / agg_total.total_page_views::numeric) * 100,2) AS pct_view_to_cart
     , agg_total.total_checkouts
     , agg_total.total_page_views - agg_total.total_checkouts AS diff_view_checkouts
-    , ROUND((agg_total.total_checkouts / agg_total.total_page_views) * 100,2) AS pct_view_checkouts
+    , ROUND((agg_total.total_checkouts::numeric / agg_total.total_page_views::numeric) * 100,2) AS pct_view_checkouts
     , agg_total.total_add_to_carts - agg_total.total_checkouts AS diff_cart_checkouts
-    , ROUND((agg_total.total_checkouts / agg_total.total_add_to_carts) * 100,2) AS pct_cart_checkouts
+    , ROUND((agg_total.total_checkouts::numeric / agg_total.total_add_to_carts::numeric) * 100,2) AS pct_cart_checkouts
 from agg_total
